1e695a525d512e59bd88414e344776af18e9bb7d
Joshua kim <joshuakim.changer@gmail.com>
2017-11-27 18:31:13 +0900

    [ADD] Design Skeleton

--- landing_web/views.py
+++ landing_web/views.py
@@ -5,11 +5,11 @@ def home_view(request):
     ctx = {
 
     }
-    return render(request, "landing_web/home.html", ctx)
+    return render(request, "landing_web/home/index.html", ctx)
 
 
 def franchise_view(request):
     ctx = {
 
     }
-    return render(request, "landing_web/franchise.html", ctx)
+    return render(request, "landing_web/franchise/index.html", ctx)
